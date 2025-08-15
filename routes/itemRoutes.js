2fc64f3e95e59bf5ecdcaf39ab1f8c4a5749f6e3
Bint Alhuda Sultan <bntalhdysltan@gmail.com>
2025-08-15 15:12:35 +0300

    added update functionality

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -6,5 +6,6 @@ router.post('/new', itemController.createItem)
 router.get('/', itemController.showAllItems)
 router.get('/:id', itemController.showItem) 
 router.delete('/:id', itemController.deleteItem) 
+router.put('/:id', itemController.updateItem) 
 
 module.exports = router
