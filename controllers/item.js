42d50fa690e075baa78b936491ec3bf26a6fdb83
Bint Alhuda Sultan <bntalhdysltan@gmail.com>
2025-08-15 15:04:16 +0300

    add show and delete item functionality

--- controllers/item.js
+++ controllers/item.js
@@ -27,9 +27,39 @@ async function showAllItems(req, res) {
     }
 }
 
+async function showItem(req,res) {
+
+    try{
+        const item = await Item.findById(req.params.id)
+        if (item){
+            res.status(200).json(item)
+        } else {
+            res.sendStatus(404)
+        }
+    }catch (err) {
+        res.status(500).json({error: err.message})
+    }
+}
+
+async function deleteItem(req,res) {
+    try{
+        const deletedItem = await Item.findByIdAndDelete(req.params.id)
+        if (deletedItem){
+            res.status(200).json(deletedItem)
+        } else {
+            res.sendStatus(404)
+        }
+    }catch (err){
+        res.status(500).json({error: err.message})
+    }
+    
+}
+
 
 
 module.exports = {
     createItem,
-    showAllItems
+    showAllItems,
+    showItem,
+    deleteItem
 }
--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -4,5 +4,7 @@ const itemController = require('../controllers/item')
 
 router.post('/new', itemController.createItem)
 router.get('/', itemController.showAllItems)
+router.get('/:id', itemController.showItem) 
+router.delete('/:id', itemController.deleteItem) 
 
 module.exports = router
