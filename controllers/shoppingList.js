3e98cf10a2cf4206aeeec48920e3dbb619b38d90
Is an English Major <Najeebihesa@gmail.com>
2025-08-15 14:52:43 +0300

    finished controllers

--- controllers/shoppingList.js
+++ controllers/shoppingList.js
@@ -1,5 +1,5 @@
 const shoppingList = require('../models/shoppingList')
-
+// to create the list
 async function createList(req,res){
     try {
         console.log('now showing', req.body)
@@ -9,7 +9,7 @@ async function createList(req,res){
         console.log(error)
     }
 }
-
+// to show the list made by the user
 async function showList(req,res){
     try {
         const list = await shoppingList.findById(req.params.id)
@@ -23,7 +23,7 @@ async function showList(req,res){
         res.status(500).json({error: error.message})
     }
 }
-
+// to show us all the shopping lists made by users - delete if not needed
 async function showAllLists (req,res){
     try {
         const allLists =  await shoppingList.find()
@@ -36,4 +36,30 @@ async function showAllLists (req,res){
         console.log(error)
         res.status(500).json({error: error.message})
     }
+}
+
+async function updateList (req,res){
+    try {
+        const list = await shoppingList.findByIdAndUpdate(req.params.id, req.body, {new:true})
+        res.status(200).json(list)
+    } catch (error) {
+        res.status(500).json({error:error.message})
+    }
+}
+
+async function deleteList (req,res){
+    try {
+        const deletedList = await shoppingList.findByIdAndDelete(req.params.id)
+        res.status(deleted)
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+}
+
+module.exports ={
+    createList,
+    showList,
+    showAllLists,
+    updateList,
+    deleteList
 }
