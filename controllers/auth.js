624d947412123cef0bb2d5eab784bdcf984ac4c5
Is an English Major <Najeebihesa@gmail.com>
2025-08-20 11:17:59 +0300

    made the backend readme file

--- controllers/auth.js
+++ controllers/auth.js
@@ -48,9 +48,10 @@ exports.login = async (req, res) => {
       id: user._id
       // Add anything else that you want to put into the JWT token here
     }
-    const token = jwt.sign(payload, SECRET, { expiresIn: '1h' }) //Look at the docs for more 'expires in' options
+    const token = jwt.sign(payload, process.env.SECRET, { expiresIn: '1h' }) //Look at the docs for more 'expires in' options
     res.json({ token })
   } catch (err) {
+    console.log(err)
     res.status(500).json({ message: 'Server error' })
   }
 }
